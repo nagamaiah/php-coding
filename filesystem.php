d08d47103cfe8628abb377fa9616b5c8b3312b77
Nagamaiah K <nagamaiah63@gmail.com>
2023-01-26 15:42:47 +0530

    filesytem changes added

--- filesystem.php
+++ filesystem.php
@@ -2,19 +2,24 @@
 echo "<h4 style='text-align:center'>Files and Paths</h4>";
 echo '<pre>';
 
-echo 'File : '.__FILE__.'<br>'; // C:\xampp\htdocs\coding\App\index.php
-echo 'Dir : '.__DIR__.'<br>'; // C:\xampp\htdocs\coding\App
-echo 'basename of dir : '.basename(__DIR__).'<br>'; // App
-echo 'basename of file : '.basename(__FILE__).'<br>'; // index.php
-echo 'basename of file without extension : '.basename(__FILE__, '.php').'<br><br>'; //index
+echo 'File : '.__FILE__.'<br>'; // C:\xampp\htdocs\php-coding\filesystem.php or /var/www/html/php-coding/filesystem.php
+echo 'Dir : '.__DIR__.'<br>'; // C:\xampp\htdocs\php-coding or /var/www/html/php-coding
+echo 'basename of dir : '.basename(__DIR__).'<br>'; // php-coding
+echo 'basename of file : '.basename(__FILE__).'<br>'; // filesystem.php
+echo 'basename of file without extension : '.basename(__FILE__, '.php').'<br><br>'; //filesystem
+echo 'dirname of dir : '.dirname(__DIR__).'<br>'; // /var/www/html
+echo 'dirname of file : '.dirname(__FILE__).'<br><br>'; // /var/www/html/php-coding
+
+echo 'realpath of dir : '.realpath(__DIR__).'<br>'; // /var/www/html/php-coding
+echo 'realpath of file : '.realpath(__FILE__).'<br><br>'; // /var/www/html/php-coding/filesystem.php
 
 echo 'DIR pathinfo : <br>';
 print_r(pathinfo(__DIR__));
 // Array
 // (
-//     [dirname] => C:\xampp\htdocs\coding
-//     [basename] => App
-//     [filename] => App
+//     [dirname] => /var/www/html
+//     [basename] => php-coding
+//     [filename] => php-coding
 // )
 
 echo "<br>";
@@ -22,32 +27,19 @@ echo 'FILE pathinfo : <br>';
 print_r(pathinfo(__FILE__)).'<br><br>';
 // Array
 // (
-//     [dirname] => C:\xampp\htdocs\coding\App
-//     [basename] => index.php
+//     [dirname] => C:\xampp\htdocs\php-coding
+//     [basename] => filesystem.php
 //     [extension] => php
-//     [filename] => index
+//     [filename] => filesystem
+// )  or 
+
+// Array
+// (
+//     [dirname] => /var/www/html/php-coding
+//     [basename] => filesystem.php
+//     [extension] => php
+//     [filename] => filesystem
 // )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
-
-
 
 
 ?>
